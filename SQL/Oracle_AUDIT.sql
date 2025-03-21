d3563daf751299669800f5d1e31384fe0533a71f
newbie01github <newbie01.others@gmail.com>
2025-03-21 17:04:08 +1300

    Update Oracle_AUDIT.sql

--- SQL/Oracle_AUDIT.sql
+++ SQL/Oracle_AUDIT.sql
@@ -1,5 +1,8 @@
--- CHECK for OBJECT Privilege 
-select username, timestamp, action_name, sys_privilege, priv_used
-  from dba_audit_trail
-order by timestamp asc  
+-- CHECK for OBJECT Privilege
+set linesize 200
+set pagesize 5000
+col terminal format a30
+select username, terminal, timestamp, action_name, sys_privilege, priv_used
+from dba_audit_trail
+order by timestamp asc
 /
